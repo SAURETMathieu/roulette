35553f913b88bc69737ec11d754d34bbcc0937fc
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-07-12 20:31:43 +0200

    feat(logout): logout component

--- src/components/buttons/logoutButton.tsx
+++ src/components/buttons/logoutButton.tsx
@@ -1,28 +1,20 @@
-"use client";
-
-//import { signOut } from "next-auth/react";
-//import { useLocale } from "next-intl";
-
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
+import { signOut } from "@/src/lib/auth/actions";
 
 export default function LogoutButton() {
-  //const locale = useLocale();
   const t = useTranslations("Navbar");
 
-  const handleSignout = async () => {
-    //await signOut({ callbackUrl: `/${locale}/login` });
-    console.log("Sign out");
-  };
-
   return (
-    <Button
+    <form action={signOut}>
+      <Button
       variant="ghost"
       className="h-fit w-full justify-start p-0"
       aria-label={t("signout")}
-      onClick={handleSignout}
     >
       {t("signout")}
     </Button>
+    </form>
+
   );
 }
