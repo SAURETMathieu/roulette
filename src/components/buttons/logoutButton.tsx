809dd0a70972f7d7d2e04d156aac807531fa63e4
SAURETMathieu <mathieu.sauret.dev@gmail.com>
2024-07-16 14:18:40 +0200

    fix(logoutButton): use supabase client for listening events

--- src/components/buttons/logoutButton.tsx
+++ src/components/buttons/logoutButton.tsx
@@ -1,20 +1,27 @@
+"use client";
+
 import { Button } from "@/components/ui/button";
 import { useTranslations } from "next-intl";
-import { signOut } from "@/src/lib/auth/actions";
+import { supabaseClient } from "@/src/lib/supabase/client";
+import { useRouter } from "next/navigation";
 
 export default function LogoutButton() {
   const t = useTranslations("Navbar");
+  const router = useRouter();
+
+  const handleLogout = async () => {
+    await supabaseClient.auth.signOut();
+    router.push('/login');
+  };
 
   return (
-    <form action={signOut}>
-      <Button
+    <Button
+      onClick={handleLogout}
       variant="ghost"
       className="h-fit w-full justify-start p-0"
       aria-label={t("signout")}
     >
       {t("signout")}
     </Button>
-    </form>
-
   );
 }
